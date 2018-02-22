56fdcbc87f53526abc963faef1d65baf666c6aef
Moses Surumen <moses@calvisitor-10-105-169-49.calvisitor.1918.berkeley.edu>
2018-02-22 10:37:42 -0800

    all

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import {createHistory as browserHistory } from 'history/createBrowserHistory';
 import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
 
+// "build": "react-scripts build && yarn run delete-maps",
+// "postbuild": "rimraf build/**/*.map",
+// "delete-maps": "yarn run delete-map-files && yarn run delete-references-to-map-files",
+// "delete-map-files": "find ./build -name '*.map' -delete",
+// "delete-references-to-map-files": "find ./build -regex '.*\\.\\(js\\|css\\)' -exec sed -i -E '\\/[\\*\\/]#\\ssourceMappingURL=main(\\.[0-9a-f]+)?\\.(css|js)\\.map(\\*\\/)?/g' {} +",
 
 ReactDOM.render(
   <Routes history={browserHistory} />,
